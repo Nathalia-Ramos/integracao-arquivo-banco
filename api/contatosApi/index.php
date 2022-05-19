07fbbfc38cb7c8f8027ff6a2d54f929527651ce9
NATHALIA APARECIDA RAMOS <21276387@CFP127ALUNO.LOCAL>
2022-05-19 17:00:29 -0300

    criando API

--- api/contatosApi/index.php
+++ api/contatosApi/index.php
@@ -6,8 +6,20 @@
     //Criando um objeto do slim chamado app, para configurar os EndPoint
     $app = new \Slim\App();
 
+    //EndPoint: Requisição para listar todos os contatos
+    $app->get('/contatos', function($request, $response, $args){
+        $response-> write('Testando')
+    });
 
+    //EndPoint: Requisição para listar todos os contatos pelo id
+    $app->get('/contatos/{id}', function($request, $response, $args){
 
+    });
+
+    //EndPoint: Requisição para inserir um novo contato
+    $app->get('/contatos', function($request, $response, $args){
+
+    });
 
 
 
